af335003cf4f805d37ee09064a48d768a24d4b60
= <=>
2022-04-11 19:50:12 +0530

    Only LeapYear Added

---
+++ SampleDataOne/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataOne
+{
+    public class LeapYear
+    {
+        public void CheckLeapYear()
+        {
+            Console.WriteLine("Enter the year you want to check:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0)
+            {
+                Console.WriteLine(+year + " is leap year\n");
+            }
+            else
+            {
+                Console.WriteLine(+year + " is not a leap year\n");
+            }
+        }
+    }
+}
