f2c95486ebc5580fd981b0b6928b7f984ea3fbad
= <=>
2022-04-11 19:42:05 +0530

    First Commit

---
+++ SampleDataOne/FlipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataOne
+{
+    public class FLipCoin
+    {
+        public void Flip()
+        {
+            int tailCount = 0, headCount = 0;
+
+            Random random = new Random();
+
+            Console.WriteLine("Enter Number of Times to Flip a Coin");
+            int time = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= time; i++)
+            {
+                double input = random.NextDouble();
+                if (input < 0.5)
+                    tailCount++;
+                else
+                    headCount++;
+            }
+            Console.WriteLine("headcount:{0} tailcount:{1}", headCount, tailCount);
+            double tail_Percentage = (tailCount * 100) / time;
+            double head_Percentage = (headCount * 100) / time;
+            Console.WriteLine("Head Percentage: " + head_Percentage + ", Tail Percentage: " + tail_Percentage + "\n");
+        }
+    }
+}
